1b64f284a07f3316cd0000b70bcfba5ca92cc10d
danielboe <danielboe@gmx.de>
2016-07-14 16:11:23 +0200

    Cleanup

--- zuno_arduino_node/zuno_arduino_node.ino
+++ zuno_arduino_node/zuno_arduino_node.ino
@@ -8,15 +8,17 @@
 
 #define DHTPIN 2     // what digital pin we're connected to
 #define DHTTYPE DHT22   // DHT 22
+
 #define FTPIN 0   // moisture sensor analog pin
+
 #define TRIGGER_PIN  12 //ultraschall distanz
 #define ECHO_PIN     11 //ultraschall distanz
 #define MAX_DISTANCE 200 //ultraschall distanz
 
 #include <TaskScheduler.h>
 #include "DHT.h"
-#include "zunonode.h"
 #include <NewPing.h>
+#include "zunonode.h"
 
 ZUNONODE transport;
 DHT dht(DHTPIN, DHTTYPE);
@@ -24,14 +26,13 @@ Scheduler runner;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 SENSOR temp;
-//SENSOR sonic;
-//SENSOR moist;
+//!SENSOR sonic;
+//!SENSOR moist;
 
 // Callback methods prototypes
 void dht_send(); //dht send
 void ft_send(); //moisture sensor send
 void sonic_send(); //sonic distance sensor send
-//intervall check task wenn änderung, dann senden
 
 Task t1(1000, TASK_FOREVER, &dht_send, &runner, true);  //adding task to the chain on creation
 Task t2(2000, TASK_FOREVER, &ft_send, &runner, true);  //adding task to the chain on creation
@@ -39,23 +40,12 @@ Task t3(2000, TASK_FOREVER, &sonic_send, &runner, true);  //adding task to the c
 
 void dht_send() {
   float h = dht.readHumidity(true);
-  float t = dht.readTemperature(false,true);  // Read temperature as Celsius (the default)
-  //float hic = dht.computeHeatIndex(t, h, false);
- /*Serial.print("loop:");
-  Serial.println(temp.test_counter(100));
-  Serial.print("h: ");
-  Serial.println(temp.test_value(1, &h, 0.5));
-  Serial.print("t: ");
-  Serial.println(temp.test_value(2 , &t, 0.5));*/
-    
-  if (isnan(h) || isnan(t)) {
-    Serial.println("error");
-    return;
-    }  // Check if any reads failed and exit early (to try again).
-
+  float t = dht.readTemperature(false,true);  // Force read in C
+   
+  //if counter or new value from sensor
   if (temp.test_counter(20)  || temp.test_value(1, &h, 0.5) || temp.test_value(2, &t, 0.5)) {
-    transport.set_values(1, (t*100));
-    transport.set_values(2, (h*100));
+    transport.set_values(1, (t*100)); //!change to ref or pointer
+    transport.set_values(2, (h*100)); //!change to ref or pointer
     transport.send_values('A');
   }
 }
@@ -73,22 +63,14 @@ void sonic_send() {
   //transport.send_values('C');
 }
 
-int freeRam () {
-   extern int __heap_start, *__brkval; 
-   int v; 
-   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
-}
-
 void setup() {
   Serial.begin(9600);
   Serial.println("zuno dht node v2");
   dht.begin();
-  
   delay(2000);
   runner.startNow();
 }
 
 void loop() {
-  //Serial.println(freeRam());
   runner.execute();
 }
